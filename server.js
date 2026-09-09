bf40be0833b34a066dd4a0868defe07638945e05
unknown <rizal.h33@gmail.com>
2026-09-09 07:55:27 +0700

    Fix Express 5 catch-all route syntax

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('{*path}', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
